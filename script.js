bbd77b21d80979a2b4affd3a6422ad7bb4bf2d6b
fathimohamed <mohafathi24@gmail.com>
2024-06-22 17:14:28 +0300

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,19 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+{
+document.body.style.backgroundcolor=color;
+}
+{
+    alert(message);
+}
+{
+    const newParagraph =document.createElement("p");
+    newParagraph.textContent=text;
+    document.body.appendChild(newParagraph);
+}
+document.addEventListener('DOMContentLoaded'),(Event) =>
+    ChangeBackgroundColor('lightblue');
+   showAlertMessage(Welcome to the website!'')
+   addParagraph('This is the a dynamically added paragraph');
+});
